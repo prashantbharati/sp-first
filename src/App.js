7c60f631577424d61f3ed136e1880ae05aba4399
prashantbharati92@gmail.com <prashantbharati92@gmail.com>
2022-03-30 22:32:30 +0530

    divs spacing done

--- src/App.js
+++ src/App.js
@@ -9,6 +9,13 @@ const App = () => {
       <div className={classes.div1}>lol</div>
 
       <div className={classes.div2}>lol2</div>
+      <div className={classes.div3}>lol2</div>
+      <div className={classes.div4}>lol2</div>
+      <div className={classes.div5}>lol2</div>
+      <div className={classes.div6}>lol2</div>
+      <div className={classes.div7}>lol2</div>
+      <div className={classes.div8}>lol2</div>
+      <div className={classes.div9}>lol2</div>
     </>
   );
 };
--- src/styles.js
+++ src/styles.js
@@ -10,4 +10,32 @@ export default makeStyles((theme) => ({
     backgroundColor: "black",
     height: "40vh",
   },
+  div3: {
+    backgroundColor: "orange",
+    height: "100vh",
+  },
+  div4: {
+    backgroundColor: "pink",
+    height: "100vh",
+  },
+  div5: {
+    backgroundColor: "violet",
+    height: "1vh",
+  },
+  div6: {
+    backgroundColor: "indigo",
+    height: "100vh",
+  },
+  div7: {
+    backgroundColor: "blue",
+    height: "100vh",
+  },
+  div8: {
+    backgroundColor: "green",
+    height: "100vh",
+  },
+  div9: {
+    backgroundColor: "yellow",
+    height: "100vh",
+  },
 }));
