938add3fe366206853157c8d1c8674fed5a1605b
prashantbharati92@gmail.com <prashantbharati92@gmail.com>
2022-03-31 21:53:41 +0530

    nav buttons done

--- src/styles.js
+++ src/styles.js
@@ -2,7 +2,7 @@ import { makeStyles } from "@material-ui/core/styles";
 
 export default makeStyles((theme) => ({
   image: {
-    marginRight: "15px",
+    // marginRight: "15px",
   },
 
   title: {
