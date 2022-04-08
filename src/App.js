373689a47e431e4697cee9f40faeec54c4a9b5ad
prashantbharati92@gmail.com <prashantbharati92@gmail.com>
2022-04-08 20:20:39 +0530

    nav added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Home from "./home";
-
+// import Practice from "./practice";
 const App = () => {
   return <Home />;
 };
--- src/practice.js
+++ src/practice.js
@@ -2,14 +2,18 @@ import React from "react";
 import { Typography, Button, Grid } from "@material-ui/core";
 const practice = () => {
   return (
-    <Grid container>
-      <Grid item md={1} style={{ backgroundColor: "green" }}>
-        lol1
+    <Grid container spacing={4}>
+      <Grid item lg={4}>
+        <h1 style={{ backgroundColor: "green", height: "250px" }}>lol1</h1>
       </Grid>
 
-      <Grid item md={11} style={{ backgroundColor: "red" }}>
-        lol2
+      <Grid item lg={4}>
+        <h1 style={{ backgroundColor: "red", height: "250px" }}>lol1</h1>
       </Grid>
+
+      {/* <Grid item md={2} style={{ backgroundColor: "orange", height: "250px" }}>
+        <h1>lol1</h1>
+      </Grid> */}
     </Grid>
   );
 };
