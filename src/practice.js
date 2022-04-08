9fbff4c5745f43d0e8cb987df65a32fd61d7dd9a
prashantbharati92@gmail.com <prashantbharati92@gmail.com>
2022-04-08 20:38:47 +0530

    div1 done complete

--- src/practice.js
+++ src/practice.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Typography, Button, Grid } from "@material-ui/core";
+import { Grid } from "@material-ui/core";
 const practice = () => {
   return (
     <Grid container spacing={4}>
